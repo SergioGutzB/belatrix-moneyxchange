057add16440da02271685d7b0b871248cdcb9e03
SergioGutzB <sergut18@gmail.com>
2019-06-25 06:54:26 -0500

    implemented ControlValueAccessor in input-currency

--- src/app/components/input-currency/input-currency.component.ts
+++ src/app/components/input-currency/input-currency.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-input-currency',
   templateUrl: './input-currency.component.html',
-  styleUrls: ['./input-currency.component.scss']
+  styleUrls: ['./input-currency.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputCurrencyComponent),
+      multi: true
+    }
+  ]
 })
-export class InputCurrencyComponent implements OnInit {
+export class InputCurrencyComponent implements OnInit, ControlValueAccessor {
+
+  value: string;
+  isDisabled: boolean;
+  onChange = (_: any) => { };
+  onTouch = () => { };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onInput(value: string) {
+    console.log('value: ', value);
+    this.value = value;
+    this.onTouch();
+    this.onChange(this.value);
+  }
+
+  writeValue(value: any): void {
+    if (value) {
+      this.value = value || '';
+    } else {
+      this.value = '';
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 
 }
